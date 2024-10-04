1c649e40252c6800758d46fccf8acb283249c6e0
Jorge Costa <jorge.costa@developer.pt>
2024-10-04 20:10:35 +0100

    include padding logic

--- packages/edit-site/src/components/global-styles/ui.js
+++ packages/edit-site/src/components/global-styles/ui.js
@@ -318,7 +318,10 @@ function GlobalStylesUI( { path, onPathChange } ) {
 			className="edit-site-global-styles-sidebar__navigator-provider"
 			initialPath="/"
 		>
-			<WrapperComponent path={ path } onPathChange={ onPathChange }>
+			<WrapperComponent
+				path={ path }
+				onPathChange={ onPathChange }
+			></WrapperComponent>
 			<GlobalStylesNavigationScreen path="/">
 				<ScreenRoot />
 			</GlobalStylesNavigationScreen>
@@ -406,9 +409,7 @@ function GlobalStylesUI( { path, onPathChange } ) {
 				<ContextScreens
 					key={ 'screens-block-' + block.name }
 					name={ block.name }
-						parentMenu={
-							'/blocks/' + encodeURIComponent( block.name )
-						}
+					parentMenu={ '/blocks/' + encodeURIComponent( block.name ) }
 				/>
 			) ) }
 
@@ -419,12 +420,7 @@ function GlobalStylesUI( { path, onPathChange } ) {
 			<GlobalStylesActionMenu />
 			<GlobalStylesBlockLink />
 			<GlobalStylesEditorCanvasContainerLink />
-<<<<<<< HEAD
 		</Navigator>
-=======
-			</WrapperComponent>
-		</NavigatorProvider>
->>>>>>> 08de320868a (Add path sync mechanism and sylebook block selection)
 	);
 }
 export { GlobalStylesMenuSlot };
