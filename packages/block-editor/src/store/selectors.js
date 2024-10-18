2ec7a03e43d76eadb071d89dec70c7a7c7fd6545
André <583546+oandregal@users.noreply.github.com>
2024-10-18 13:07:11 +0200

    Be explicit about the check

--- packages/block-editor/src/store/selectors.js
+++ packages/block-editor/src/store/selectors.js
@@ -1595,7 +1595,7 @@ const isBlockVisibleInTheInserter = (
 	checkedBlocks.add( blockName );
 
 	// If parent blocks are not visible, child blocks should be hidden too.
-	if ( !! blockType.parent?.length ) {
+	if ( Array.isArray( blockType?.parent ) ) {
 		return blockType.parent.some(
 			( name ) =>
 				( blockName !== name &&
